f9b426b386f5bba0f668179e07cc4ff12e80cc3e
Saisha2209 <76441200+Saisha2209@users.noreply.github.com>
2021-05-01 11:23:25 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,13 @@
+class BLOCKPINK extends Base{
+    constructor(x,y,width,height){
+super(x,y,30,50)
+    }
+    display(){
+       
+        super.display();
+        stroke("black")
+        fill("pink")
+        super.display();
+  
+    }
+}
